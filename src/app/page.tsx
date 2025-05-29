312b0d131bc89d2e2fb1e16e72740c54bfb3a537
Emrecan Erbay <emrerbay.st@gmail.com>
2025-05-29 21:21:29 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { redirect } from 'next/navigation';
 
 const GOOGLE_FORMS = [
-  'https://docs.google.com/forms/d/1fNApmsQycPhDOdnqdFnHKMfh2Fe6JJpRa11fNyl82lU/viewform',
-  'https://docs.google.com/forms/d/1X5z4z3f8ErzV4aRBkbGFZpoOt5l0Da-gxpGxu4D-f2A/viewform',
-  'https://docs.google.com/forms/d/1oKr5FyV0jkI4epB9H_Qjc256EpTr74hzmFth2ErR7a8/viewform',
+  'https://docs.google.com/forms/d/1fNApmsQycPhDOdnqdFnHKMfh2Fe6JJpRa11fNyl82lU/viewform',//1
+  'https://docs.google.com/forms/d/1X5z4z3f8ErzV4aRBkbGFZpoOt5l0Da-gxpGxu4D-f2A/viewform',//2
+  'https://docs.google.com/forms/d/1oKr5FyV0jkI4epB9H_Qjc256EpTr74hzmFth2ErR7a8/viewform',//3
 ];
 
 export default function Home() {
@@ -14,6 +14,4 @@ export default function Home() {
   // Seçilen forma yönlendir
   redirect(randomForm);
 
-  // Bu kod asla çalışmayacak, çünkü redirect işlevi bir exception fırlatır
-  return null;
 }
